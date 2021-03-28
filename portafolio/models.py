9aa2b98aa3c625c0db3327f67557eb0170443bf6
Laptop - Omar <omar.angarita.j@gmail.com>
2021-03-28 12:42:11 -0500

    Modificando la interfaz admin

--- portafolio/models.py
+++ portafolio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Proyecto'
+        verbose_name_plural = 'Proyectos'
+    
+    def __str__(self):
+        return self.title
